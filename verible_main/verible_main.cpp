998fe34c004c486410f997e3415aaf7a88090210
Gary Baugh <baughg@tcd.ie>
2020-04-25 22:00:59 +0100

    Initial commit

---
+++ verible_main/verible_main.cpp
@@ -0,0 +1,26 @@
+// verible_main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+#include "absl/flags/flag.h"
+#include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/string_view.h"
+#include "absl/types/span.h"  // for MakeArraySlice
+
+int main(int argc, char** argv) {
+	const auto usage =
+		absl::StrCat("usage: ", argv[0], " [options] <file> [<file>...]");
+	//const auto args = verible::InitCommandLine(usage, &argc, &argv);
+    
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
